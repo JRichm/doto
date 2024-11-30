4a54e7b8b0a8a8dde518dcc82138bf2c7cb0e3ee
JRichm <jblackenmusic@gmail.com>
2024-11-30 16:56:43 -0700

    add newpage to test multiple endpoints

---
+++ src/app/newpage/page.tsx
@@ -0,0 +1,10 @@
+import Header from "../components/header"
+
+export default function NewPage() {
+    return (
+        <div>
+            <Header />
+            <p>this is my div</p>
+        </div>
+    )
+}
